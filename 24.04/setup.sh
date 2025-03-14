f50a9bb06b58df426344bdb694a9cd49dafcec0e
Maksim Evtyagin <evtyagin.maksim@gmail.com>
2025-03-14 17:37:34 +0200

    Add pcov PHP extension

--- 24.04/setup.sh
+++ 24.04/setup.sh
@@ -33,7 +33,7 @@ function php_install() {
     apt update -y
     add-apt-repository ppa:ondrej/php
     apt install -y php${PHP_VERSION} php${PHP_VERSION}-common php${PHP_VERSION}-cli
-    apt install -y php${PHP_VERSION}-{bz2,curl,mbstring,intl,xml,zip,xdebug,apcu,redis,pdo_mysql}
+    apt install -y php${PHP_VERSION}-{bz2,curl,mbstring,intl,xml,zip,xdebug,apcu,redis,pdo_mysql,pcov}
     php -v
     php -m
 }
